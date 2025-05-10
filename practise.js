fed2812eb09585f0c7de0d4695e32a868ed8278e
Rimon <mdraisul.i.rimon@gmail.com>
2025-05-10 21:46:30 +0600

    updated

---
+++ practise.js
@@ -0,0 +1,7 @@
+let name=prompt("Enter the name of the product: ");
+let id=parseInt(prompt("Enter the product id: "));
+let date=prompt("Enter the date of you want this: ");
+
+console.log(`Product name: ${name}`);
+console.log(`Product id: ${id}`);
+console.log(`Delivery date: ${date}`);
